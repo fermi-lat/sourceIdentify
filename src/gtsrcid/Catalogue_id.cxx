1cea85011ca3c071e6527be0d2be8ceaae28281d
jurgen <>
2007-10-02 22:01:16 +0000

    Change parameter name maxNumCtp to maxNumCpt

--- src/gtsrcid/Catalogue_id.cxx
+++ src/gtsrcid/Catalogue_id.cxx
@@ -1,10 +1,13 @@
 /*------------------------------------------------------------------------------
-Id ........: $Id: Catalogue_id.cxx,v 1.5 2007/09/21 14:29:03 jurgen Exp $
+Id ........: $Id: Catalogue_id.cxx,v 1.6 2007/09/21 20:27:14 jurgen Exp $
 Author ....: $Author: jurgen $
-Revision ..: $Revision: 1.5 $
-Date ......: $Date: 2007/09/21 14:29:03 $
+Revision ..: $Revision: 1.6 $
+Date ......: $Date: 2007/09/21 20:27:14 $
 --------------------------------------------------------------------------------
 $Log: Catalogue_id.cxx,v $
+Revision 1.6  2007/09/21 20:27:14  jurgen
+Correct cfits_collect bug (unstable row selection)
+
 Revision 1.5  2007/09/21 14:29:03  jurgen
 Correct memory bug and updated test script
 
@@ -528,8 +531,8 @@ Status Catalogue::cid_refine(Parameters *par, long iSrc, Status status) {
       }
 
       // Apply the maximum number of counterpart threshold
-      if (numUseCC > par->m_maxNumCtp)
-        numUseCC = par->m_maxNumCtp;
+      if (numUseCC > par->m_maxNumCpt)
+        numUseCC = par->m_maxNumCpt;
 
       // Eliminate counterpart candidates below threshold. Fall through if no 
       // counterparts are left
