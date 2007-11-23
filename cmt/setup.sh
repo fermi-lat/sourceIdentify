b9a82bd452c4e7ea499d417af3140711bbd4cb97
jurgen <>
2007-11-23 15:34:12 +0000

    Removed rh9_gcc32 directory

--- cmt/setup.sh
+++ cmt/setup.sh
@@ -1,4 +1,4 @@
-# echo "Setting sourceIdentify v1r3p1 in /home/glast/dev"
+# echo "Setting sourceIdentify v1r3p2 in /home/glast/dev"
 
 if test "${CMTROOT}" = ""; then
   CMTROOT=/opt/projects/glast/tools/CMT/v1r16p20040701; export CMTROOT
@@ -7,6 +7,6 @@ fi
 
 tempfile=`${CMTROOT}/mgr/cmt build temporary_name -quiet`
 if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt -quiet setup -sh -pack=sourceIdentify -version=v1r3p1 -path=/home/glast/dev  $* >${tempfile}; . ${tempfile}
+${CMTROOT}/mgr/cmt -quiet setup -sh -pack=sourceIdentify -version=v1r3p2 -path=/home/glast/dev  $* >${tempfile}; . ${tempfile}
 /bin/rm -f ${tempfile}
 
